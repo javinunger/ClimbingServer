36d128ed600cb77ec73418b4ebd7eb115952047a
Chris Dilley <cpd5@students.calvin.edu>
2015-11-20 15:38:27 -0500

    Update initialSchema.sql

--- Schema/initialSchema.sql
+++ Schema/initialSchema.sql
@@ -1,4 +1,4 @@
-﻿----------------------------------------------------------------
+----------------------------------------------------------------
 -- This SQL script builds the SQL tables for our Climbing App --
 ----------------------------------------------------------------
 -- @author David Michel ----------------------------------------
@@ -7,7 +7,7 @@
 ----------------------------------------------------------------
 
 -- Drop previous versions of the tables if they they exist, in reverse order of foreign keys.
-DROP TABLE IF EXISTS ClimberClimb;
+--DROP TABLE IF EXISTS ClimberClimber;
 DROP TABLE IF EXISTS Climb;
 DROP TABLE IF EXISTS Climber;
 
@@ -16,40 +16,58 @@ CREATE TABLE Climber (
 	ID varchar(50) PRIMARY KEY, -- Unique identifier, ignore case (i.e. 'davejoshmike')
 	password varchar(50), -- ENCRYPTED. password must be encrypted, no less than 6 characters, case sensitive
 	emailAddress varchar(50), -- Email used to login (can be NULL if using facebook?)
-	name varchar(50), -- first and last name seperated by a comma
-	myFriends varchar(500), -- store multiple userIDs in one string seperated by commas? i.e. myFriends = 'davejoshmike, cpd5, jam'
+	name varchar(50), -- last and first name seperated by a comma
+	--myFriends varchar(500), -- store multiple userIDs in one string seperated by commas? i.e. myFriends = 'davejoshmike, cpd5, jam'
 	dateCreated timestamp -- Member since: dateCreated
 	);
 
 CREATE TABLE Climb (
 	ID integer PRIMARY KEY, -- unique identifier
+	climberID varchar(50) REFERENCES Climber(ID),
 	name varchar(50), -- name given by user
 	color varchar(50), -- color selected by user (default = 'white')
 	difficulty varchar(50), -- i.e. '5.8+', 'V7', or '5.10a'
 	type varchar(50), -- i.e. 'boulder' 'top rope'
 	notes varchar(500), -- a paragraph of notes (500 character limit)
-	location varchar(50), -- where the climb took place
+	--location varchar(50), -- where the climb took place
 	time timestamp -- when the climb happened (initially auto-generated, but editable by the user)
 	);
 
--- Linking table for Climber and Climb tables
-CREATE TABLE ClimberClimb (
-	climberID varchar(50) REFERENCES Climber(ID),
-	climbID integer REFERENCES Climb(ID)
-	);
+-- Friends Table (not implimented)
+--CREATE TABLE ClimberClimber (
+--	climberID varchar(50) REFERENCES Climber(ID),
+--	climbID integer REFERENCES Climb(ID)
+--	);
 
 -- Allow users to select data from the tables.
 GRANT SELECT ON Climber TO PUBLIC;
 GRANT SELECT ON Climb TO PUBLIC;
-GRANT SELECT ON ClimberClimb TO PUBLIC;
+--GRANT SELECT ON ClimberClimber TO PUBLIC;
 
 -- Add sample records.
-INSERT INTO Climber VALUES ('davejoshmike', 'funthings8', 'davejoshmike@gmail.com', 'Michel, David', 'jam, cpd5', '2015-10-27 18:10:00');
+INSERT INTO Climber VALUES ('davejoshmike', 'funthings8', 'davejoshmike@gmail.com', 'Michel, David', '2015-10-27 18:10:00');
+INSERT INTO Climber VALUES ('cpd5', 'funthings8', 'cpd5@gmail.com', 'Dilley, Chris', '2015-11-18 18:49:00');
+INSERT INTO Climber VALUES ('AustinS', 'funthings8', 'austin@gmail.com', 'S, Austin', '2015-11-18 18:50:00');
+
+INSERT INTO Climb VALUES (1, 'davejoshmike', 'route1', 'blue', '5.8+', 'top rope', 'fun climb!!!', '2015-11-18 18:49:00');
+INSERT INTO Climb VALUES (2, 'davejoshmike', 'route2', 'green', '5.8+', 'top rope', 'fun climb!!!', '2015-11-18 18:49:01');
+INSERT INTO Climb VALUES (3, 'davejoshmike', 'route3', 'yellow', '5.8+', 'top rope', 'fun climb!!!', '2015-11-18 18:49:02');
+
+INSERT INTO Climb VALUES (4, 'cpd5', 'route1', 'red', '3', 'bouldering', 'fun climb!!!', '2015-11-18 18:49:03');
+INSERT INTO Climb VALUES (5, 'cpd5', 'route2', 'green', '5.8+', 'top rope', 'fun climb!!!', '2015-11-18 18:49:04');
+INSERT INTO Climb VALUES (6, 'cpd5', 'route3', 'blue', '5.8+', 'top rope', 'fun climb!!!', '2015-11-18 18:49:05');
+
+INSERT INTO Climb VALUES (7, 'AustinS', 'route1', 'pink', '5.8+', 'top rope', 'fun climb!!!', '2015-11-18 18:39:06');
+INSERT INTO Climb VALUES (8, 'AustinS', 'route2', 'purple', '5.8+', 'top rope', 'fun climb!!!', '2015-11-18 18:49:07');
+INSERT INTO Climb VALUES (9, 'AustinS', 'route3', 'yellow', '5.8+', 'top rope', 'fun climb!!!', '2015-11-18 18:59:08');
 
 -- Test queries
 
 -- Lists the names of all climbers and how many days they've been a member
 SELECT Climber.name as Name, 
        date_part('day', NOW() - Climber.dateCreated) as Member_days
-FROM Climber
+FROM Climber;
 
+SELECT *
+FROM Climber
+WHERE id= 'davejoshmike';
